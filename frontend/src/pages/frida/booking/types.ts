ecbd1d3acb6560ddf2b036d444e6c5717f8ff3d8
fridaenoksen-dev <hello@fridaenoksen.no>
2026-04-09 20:57:45 +0200

    Satt opp nytt respetory med tidligere html + css

---
+++ frontend/src/pages/frida/booking/types.ts
@@ -0,0 +1,11 @@
+export type Bookings = {
+  id: number;
+  userId: number;
+  roomId: number;
+  fromDate: string;
+  toDate: string;
+  status: string;
+  message: string;
+  created: string;
+  uppdated: string;
+};
---
+++ frontend/src/pages/frida/room/room.ts
@@ -0,0 +1,14 @@
+export type Rooms = {
+  id: number;
+  name: string;
+  pricePrNight: number;
+  description: string;
+  features: string;
+  maxGuests: number;
+  reviews: number;
+  userId: number;
+  rating: number;
+  comment: string;
+  created: string;
+  updated: string;
+};
