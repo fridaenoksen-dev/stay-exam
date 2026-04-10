4e9ebc74336ea06eafadd5c6600ecab2f4232746
hawrami01-byte <das.haw319@gokstadakademiet.no>
2026-04-10 10:49:49 +0200

    refresh start av TS

---
+++ frontend/src/pages/dastan/request.ts
@@ -0,0 +1,7 @@
+//Dastan Salah Hawrami//
+
+
+import { Room } from "./type";
+
+const api = "http://loclahost:3000/api/"
+
---
+++ frontend/src/pages/dastan/type.ts
@@ -0,0 +1,17 @@
+// Dastan Salah Hawrami //
+
+
+export type room = {
+    id: nummer;
+    rom: string;
+    name:string;
+    price:number;
+    avalibale: boolean;
+
+}
+
+export type roomSearch {
+    checkIn: string;
+    checkOut: string;
+    personer: number;
+}
